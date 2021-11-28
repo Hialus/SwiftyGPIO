8d2b025d23b8b256668e58ae6c7871344e089b9a
Timor Morrien <hiala@gmx.de>
2021-11-28 12:56:59 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyGPIO"
+    name: "SwiftyGPIO",
+    products: [
+        .library(
+            name: "SwiftyGPIO",
+            targets: ["SwiftyGPIO"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SwiftyGPIO",
+            path: "Sources"
+        )
+    ]
 )
